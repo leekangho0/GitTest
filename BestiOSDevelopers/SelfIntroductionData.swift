44fbf0534b9c96e3c1e6ea2f443b06ecd8676ccc
jinny <jokim6982@gmail.com>
2025-05-08 17:29:32 +0900

    이강호님 수정 커밋

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -116,7 +116,7 @@ let people: [SelfIntroduction] = [
         mbti: "INFP",
         strength: "공삳",
         sns: "SNS info",
-        etc: "열심히 수료하고 취업하겠습니다!",
+        etc: "열심히 수료하고 취업하겠습니다!화이팅입니다",
         job: "디지털 노마드"
     ),
     SelfIntroduction(
@@ -436,7 +436,6 @@ let people: [SelfIntroduction] = [
         mbti: "INTJ",
         strength: "끈기와 추리력",
         sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
         etc: "열심히 수료하고 취업하겠습니다! 화이팅입니다",
         job: "그냥 iOS 개발자"
     ),
