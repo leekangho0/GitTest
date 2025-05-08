e0bca0225bacae0cce6bfb9cb38a5823150329a0
drfranken <daranic9@gmail.com>
2025-05-08 18:03:30 +0900

    이치훈 수정

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -436,7 +436,7 @@ let people: [SelfIntroduction] = [
         mbti: "INTJ",
         strength: "끈기와 추리력",
         sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
+        etc: "열심히 수료하고 취업하겠습니다!!!",
         job: "그냥 iOS 개발자"
     ),
     SelfIntroduction(
